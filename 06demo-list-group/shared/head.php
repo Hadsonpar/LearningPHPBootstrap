c26a4670897d417dc310db3ce66fe2073b9159c5
Hadsonpar <hadson1@gmail.com>
2023-07-14 08:04:12 -0500

    Sixth demo Bootstrap: List Group dinámico con PHP y MariaDB

---
+++ 06demo-list-group/shared/head.php
@@ -0,0 +1,18 @@
+<head>    
+    <!-- <meta charset="utf-8"> -->
+    <meta charset="utf-8"/>    
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Accordion with PHP and MariaDB Demo 02">
+    <meta name="author" content="Hadson Paredes C">
+    <meta name="generator" content="2023/07/08 - PE">
+        
+    <title>Accordion with PHP and MariaDB Demo 05 · Bootstrap v5.2</title>
+    <link rel="canonical" href="http://blog.hadsonpar.com/2023/07/bootstrap-carousel-dinamico-con-php-y.html">
+
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">    
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">    
+
+  </head>
